5c702907cfad59a0a243ffcbde0939d6617b966e
Bill Barnett <bill@agilo.us>
2020-12-02 16:08:53 -0500

    Fix sematics

--- day-02/question-01.exs
+++ day-02/question-01.exs
@@ -7,18 +7,18 @@ defmodule Day2Question1 do
     count >= String.to_integer(min) && count <= String.to_integer(max)
   end
 
-  def invalid_passwords(password_array) do
+  def valid_passwords(password_array) do
     count = password_array
             |> Enum.filter(&password_valid?/1)
             |> Enum.count()
-    IO.puts("#{count} invalid passwords.")
+    IO.puts("#{count} valid passwords.")
   end
 
   def work() do
     {:ok, contents} = File.read('./day-02/input.txt')
 
     String.split(contents, "\n", trim: true)
-    |> invalid_passwords()
+    |> valid_passwords()
   end
 end
 
