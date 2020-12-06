24db35625b428f4f63b981c3dbd3885c097971c6
Bill Barnett <bill@gaslight.co>
2020-12-06 12:26:10 -0500

    Minimal test should test “work” function

--- test/day-04/question-01_test.exs
+++ test/day-04/question-01_test.exs
@@ -1,19 +1,13 @@
 defmodule Day4Question1Test do
   use ExUnit.Case
 
-  describe "valid_passports/1" do
+  describe "work/1" do
     test "returns the proper answer for the test inputs" do
-      {:ok, contents} = File.read("./test/support/files/day-04/test_input.txt")
-      passports = String.split(contents, "\n\n", trim: true)
-
-      assert Day4Question1.valid_passports(passports) == 2
+      assert Day4Question1.work("./test/support/files/day-04/test_input.txt") == 2
     end
 
     test "returns the proper answer for the actual inputs" do
-      {:ok, contents} = File.read("./lib/day-04/input.txt")
-      passports = String.split(contents, "\n\n", trim: true)
-
-      assert Day4Question1.valid_passports(passports) == 260
+      assert Day4Question1.work("./lib/day-04/input.txt") == 260
     end
   end
 end
